e844f15d2929fb2e69c59427f0aab963461e5083
Adel <adelnmo2003@gmail.com>
2024-10-13 04:26:19 +0330

    final code

--- js/script.js
+++ js/script.js
@@ -1,9 +1,83 @@
 "use strict";
 
-const list = document.querySelector(".list");
-const listItem = document.querySelectorAll(".list-item");
-const result = document.querySelector(".content");
+const link = document.querySelector(".section-5-link");
+const section5 = document.querySelector(".section-5");
 
+link.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  console.log("link clicked");
+
+  section5.scrollIntoView({
+    behavior: "smooth",
+  });
+});
+
+/*
+document.body.addEventListener("keydown", function (e) {
+  console.log(e.key);
+
+  if (e.key === "Enter") {
+    document.body.style.backgroundColor = "gray";
+  } else {
+    console.log("nooo");
+  }
+});
+
+// const body = document.body;
+
+// document.querySelector(".user-name").addEventListener("keydown", function (e) {
+//   console.log(e.key);
+
+//   if (e.key === " ") {
+//     body.style.backgroundColor = "gray";
+//   } else {
+//     body.style.backgroundColor = "white";
+//   }
+// });
+*/
+
+/*
+const form = document.querySelector("form");
+const nameInput = document.querySelector(".user-name");
+const ageInput = document.querySelector(".user-age");
+
+let users = [];
+
+// 1. add an eventListener with "submit" type
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  // 2. create object
+  const newUser = {
+    name: nameInput.value,
+    age: Number.parseInt(ageInput.value),
+  };
+
+  // 3. push object into users Array
+  users.push(newUser);
+
+  // 4. clear inputs
+  nameInput.value = ageInput.value = "";
+
+  // 5. log users Array
+  console.log(users);
+});
+*/
+
+/*
+const result = document.querySelector(".content");
+const showClicked = function () {
+  console.log("clicked on the list");
+
+  // remove eventListener
+  result.removeEventListener("click", showClicked);
+};
+
+result.addEventListener("click", showClicked);
+*/
+
+/*
 // PART 1
 // 1. add eventListener to all items
 listItem.forEach((item) => {
@@ -30,3 +104,4 @@ buttonEl.addEventListener("click", function () {
 
 // 4. insert button into html
 document.body.prepend(buttonEl);
+*/
